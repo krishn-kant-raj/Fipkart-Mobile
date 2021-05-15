8255ed71535c8d7f10d74f35340bdc2ec98a59b0
krishn kant raj <krishnkantraj@gmail.com>
2021-05-16 01:13:07 +0530

    Download bug fixed and Image added

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ Price = []
 Rating = []
 Review = []
 Links = []
+Image = []
 
 def main():
 
@@ -35,14 +36,17 @@ def main():
             brand = brand[0]
         except IndexError as er:
             pass
+        
         try:
 #     Dynamic URL
             flipkart_url = f'https://www.flipkart.com/mobiles/pr?sid=tyy%2C4io&otracker=categorytree&p%5B%5D=facets.brand%255B%255D%3D{brand}&otracker=nmenu_sub_Electronics_0_{brand}'.format(brand)
         except InvalidSchema as er:
             st.write(er)
             st.write('Invalid Input')
+            
         if st.checkbox("Show URL"):
             st.write(flipkart_url)
+            
         if st.checkbox("Enter Number of Page to scrap"):
             try:    
                 pg_num = st.text_input("Enter Numbers of Page to scrap: ",1)
@@ -63,6 +67,7 @@ def main():
                     price = soup.find_all('div',{"class":"_30jeq3 _1_WHN1"})
                     ratings_reviews = soup.find_all('span',{"class":"_2_R_DZ"})
                     links = soup.find_all('a',{'class':'_1fQZEK'})
+                    img = soup.find_all('img',{'class':'_396cs4 _3exPp9'})
                     st.write("*[INFO] Phones in Page *"+str(i)+'* is *'+str(len(name)))
 
                     for i in name:
@@ -76,10 +81,12 @@ def main():
                     for i in links:
                         linktext = 'https://www.flipkart.com'+i.get('href')
                         # BitlyError due to Monthly Limit Exist
-                        #short_url = access.shorten(linktext) 
-                        #Links.append(short_url['url'])
+                        # short_url = access.shorten(linktext) 
+                        # Links.append(short_url['url'])
                         Links.append(linktext)
-
+                    for i in img:
+                        imglink = i.get('src')
+                        Image.append(imglink)
 
                 st.write("*[INFO] Raw data scraped.*")
                 
@@ -89,6 +96,7 @@ def main():
                         st.write('**Rating Count=**',len(Rating))
                         st.write('**Review Count=**',len(Review))
                         st.write('**Links Count=**',len(Links))
+                        st.write('**Image Count=**',len(Image))
                         
                 if st.checkbox("Check Missing Data"):
                     if (len(Name)==len(Price) and len(Name)==len(Rating) and len(Name)==len(Review) and len(Name)==len(Links)):
@@ -110,6 +118,11 @@ def main():
                                 for i in range(0,(len(Name)-len(Links))):
                                     Links.append('None')
                             st.write("**Missing Data Filled with 'None'**")
+
+                            if st.checkbox("Remove None Rows"):
+                                data = data[~data.Rating.str.contains("None")]
+                                st.write("**Rows having 'None' values are removed.**")
+                                st.write(data)
 
                             if st.checkbox("Show Count"):
                                 st.write("Count after filled missing data")
@@ -165,7 +178,6 @@ def main():
                     if st.checkbox("Show Head"):
                         st.write(data.head(5))
 
-
                     if st.checkbox("Show Tail"):
                         st.write(data.tail(5))
 
@@ -180,26 +192,24 @@ def main():
                         selected_columns = st.multiselect("Select Columns",all_columns)
                         new_df = data[selected_columns]
                         st.dataframe(new_df)
-
-                    if st.checkbox("Remove None Rows"):
-                        data = data[~data.Rating.str.contains("None")]
-                        st.write("**Rows having 'None' values are removed.**")
-                        st.write(data)
                         
                     if st.checkbox("Convert to Numeric type"):
+                        all_columns = data.columns.to_list()
                         selected_col = st.multiselect("Select Numeric Columns",all_columns)
                         data[selected_col] = data[selected_col].astype(int)
                         st.write(selected_col, "Converted to numeric value")
                                 
+                    if st.checkbox("Show Product Images"):
+                        for i in range(len(Image)):
+                            st.image(Image[i], width=100, caption=data['Product_Name'][i])
 
+                        
                     download = st.button('Download CSV File')
                     if download:
-                        filename = st.text_input("Enter Filename : ")
-                        filename = filename+'.csv'
                         'Download Started!'
                         csv = data.to_csv(index=False)
                         b64 = base64.b64encode(csv.encode()).decode()  # some strings
-                        linko= f'<a href="data:file/csv;base64,{b64} download={filename}">Download csv file</a>'
+                        linko= f'<a href="data:file/csv;base64,{b64}" download="flipkart-mobile.csv">Download csv file</a>'
                         st.markdown(linko, unsafe_allow_html=True)
     
 if __name__ == '__main__':
