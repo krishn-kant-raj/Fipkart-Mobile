6331f1bdfa3fbcb7f16528d9ec792d225688c28d
krishn kant raj <krishnkantraj@gmail.com>
2021-05-22 15:02:37 +0530

    Spcrapping of Specifications and some changes in interface. Scrapping of specifications will take some time but give you more information about a phone

--- app.py
+++ app.py
@@ -4,22 +4,23 @@ import streamlit as st
 import requests
 from bs4 import BeautifulSoup
 import base64
-#import bitly_api
 
-# Bitly Access Token
-#BITLY_ACCESS_TOKEN = 'access token'
-#access = bitly_api.Connection(access_token = BITLY_ACCESS_TOKEN)
+# Create empty lists
+Name = []
+Price = []
+Rating = []
+Review = []
+Links = []
+Image = []
+Rating_points = []
+Storage = []
+Battery = []
+Camera = []
+Processor = []
+Warranty = []
+Display = []
 
 def main():
-    # Create empty lists
-    Name = []
-    Price = []
-    Rating = []
-    Review = []
-    Links = []
-    Image = []
-    Rating_points = []
-    
     header_text="""
     <style>
     .column {
@@ -62,8 +63,9 @@ def main():
       </div>
     </div>
     """
+    
     st.markdown(header_text,unsafe_allow_html=True)
-    st.markdown("![img](https://i.imgur.com/dqOEkud.png)")
+    st.markdown("![img](https://www.freepnglogos.com/uploads/flipkart-logo-png/flipkart-com-logo-daily-current-affairs-capsule-january-16.png)")
 
     
     activities = ["Collect Data","Analyse"]    
@@ -89,7 +91,7 @@ def main():
             try:
                 brand = st.text_input("Enter Mobile Brand Name (See examples in sidebar) ","")
                 if not brand:
-                  st.warning('Please input a name.')
+                  st.warning('Please input a mobile brand name.')
                 brand = brand.title().split()
                 brand = brand[0]
             except IndexError as er:
@@ -104,7 +106,7 @@ def main():
             if brand !="":    
                 st.write('Verify your data on below link')
                 st.write(flipkart_url)
-                pg_num = st.slider('Select a range of values',1, 30,step=1)
+                pg_num = st.number_input('Enter how many page you want to scrap',step=1,min_value=1,max_value=10)
                 if st.checkbox("Start Scraping"):
                     for i in range(1,pg_num+1):
                         url = flipkart_url+"&page="+str(i)
@@ -119,14 +121,49 @@ def main():
                         if len(name)==0:
                             st.write('**Invalid Brand Name or Data is not available on page**',i)
                             break
-                        
+
                         price = soup.find_all('div',{"class":"_30jeq3 _1_WHN1"})
                         ratings_reviews = soup.find_all('span',{"class":"_2_R_DZ"})
                         links = soup.find_all('a',{'class':'_1fQZEK'})
                         rt_point = soup.find_all('div',{'class':'_3LWZlK'})
                         img = soup.find_all('img',{'class':'_396cs4 _3exPp9'})
+                        spec = soup.find_all('div',{'class':'fMghEO'})
                         st.write("**[Scraped]** *Phones in Page *"+str(i)+'* is *'+str(len(name)))
-
+                        st.write("**Specification** *Scrapping...*")
+                        for link in range(len(links)):
+                            spec_url = 'https://www.flipkart.com'+links[link].get('href')
+                            Specification = requests.get(spec_url)
+                            content = BeautifulSoup(Specification.content, 'html.parser')
+                            speci = content.find(class_="_2418kt")
+                            try:
+                                display = str(speci.select("li:nth-child("+str(2)+")"))
+                                dips_text = display.replace('[<li class="_21Ahn-">',"")
+                                disp_text = dips_text.replace('</li>]',"")
+                                Display.append(disp_text)
+                            except AttributeError:
+                                Display.append('Not Available')
+                                
+                            brand_list = ['Samsung','Karbonn','Lava','Nokia']
+                            if brand not in brand_list:
+                                camera = str(speci.select("li:nth-child("+str(3)+")"))
+                                cam_text = camera.replace('[<li class="_21Ahn-">',"")
+                                cam_text = cam_text.replace('</li>]',"")
+                                Camera.append(cam_text)
+                                battery_cap = str(speci.select("li:nth-child("+str(4)+")"))
+                                bat_text = battery_cap.replace('[<li class="_21Ahn-">',"")
+                                bat_text = bat_text.replace('</li>]',"")
+                                Battery.append(bat_text)
+                                processor = str(speci.select("li:nth-child("+str(5)+")"))
+                                proc_text = processor.replace('[<li class="_21Ahn-">',"")
+                                proc_text = proc_text.replace('</li>]',"")
+                                Processor.append(proc_text)
+                            warranty = content.find(class_='_352bdz')
+                            Warranty.append(warranty.text.replace('Know More',""))
+                            
+                        for ram in spec:
+                            if "Display" in ram.ul.li.text:
+                                Storage.append('0 GB RAM | 0 GB ROM')
+                            Storage.append(ram.ul.li.text)
                         for i in name:
                             Name.append(i.text)
                         for i in price:
@@ -136,20 +173,16 @@ def main():
                             Review.append(i.span.text)
                         for i in rt_point:
                             Rating_points.append(i.text)
-                        #st.write('[INFO] Shortening link...')
                         for i in links:
                             linktext = 'https://www.flipkart.com'+i.get('href')
-                            # BitlyError due to Monthly Limit Exist
-                            # short_url = access.shorten(linktext)
-                            # Links.append(short_url['url'])
                             Links.append(linktext)
                         for i in img:
                             imglink = i.get('src')
                             Image.append(imglink)
-                            
                         if len(name)<24:
                             st.write('**All available data of **',brand,'** mobile has scraped**')
-                            break    
+                            break
+                            
                     if len(Name)!=0:
                         if len(Name)==0:
                             st.write('*Try again! Nothing Scraped.*')
@@ -207,19 +240,53 @@ def main():
                                 clean_rating.append(Rating_str)
                         st.write(clean_rating[:5])
 
-                        data = {
-                            'Product_Name':Name,
-                            'Price':clean_price,
-                            'Rating':Rating_points,
-                            'Total_Ratings':clean_rating,
-                            'Total_Reviews':clean_review,
-                            'Product Link':Links,
-                            'Image Link':Image
-                        }
+##                        st.write('Name', len(Name))
+##                        st.write('claen_price',len(Price))
+##                        st.write('Rating Point',len(Rating_points))
+##                        st.write('Ratings',len(clean_rating))
+##                        st.write('Reviews',len(clean_review))
+##                        st.write('Display',len(Display))
+##                        st.write('Warranty',len(Warranty))
+##                        st.write('Storage',len(Storage))
+##                        st.write('Links',len(Links))
+##                        st.write('Image',len(Image))
+##                        st.write('Processor',len(Processor))
+##                        st.write('Battery',len(Battery))
+##                        st.write('Camera',len(Camera))
+
+                        if brand in brand_list:
+                            data = {
+                                    'Product_Name':Name,
+                                    'Price':clean_price,
+                                    'Rating':Rating_points,
+                                    'Total_Ratings':clean_rating,
+                                    'Total_Reviews':clean_review,
+                                    'Storage':Storage,
+                                    'Display':Display,
+                                    'Warranty':Warranty,
+                                    'Product_link':Links,
+                                    'Image_link': Image
+                            }
+                        else:
+                            data = {
+                                    'Product_Name':Name,
+                                    'Price':clean_price,
+                                    'Rating':Rating_points,
+                                    'Total_Ratings':clean_rating,
+                                    'Total_Reviews':clean_review,
+                                    'Storage':Storage,
+                                    'Display':Display,
+                                    'Camera':Camera,
+                                    'Battery':Battery,
+                                    'Processor':Processor,
+                                    'Warranty':Warranty,
+                                    'Product_link':Links,
+                                    'Image_link': Image
+                                }
                         try:
                             data = pd.DataFrame(data)
                         except:
-                            st.print('All columns are not of same length')
+                            st.write('All columns are not of same length')
                             
                         #data = data[~data.Total_Ratings.str.contains("None")]
                         data[['Price','Total_Ratings','Total_Ratings']] = data[['Price','Total_Ratings','Total_Ratings']].astype(int)
@@ -238,7 +305,7 @@ def main():
             df = pd.read_csv(data)
             st.dataframe(df.head())
             try:
-                show_df = df.loc[:, 'Product_Name':'Total_Reviews']
+                show_df = df.loc[:, 'Product_Name':'Warranty']
                 st.write('Product Link and Image Link Removed from View')
                 
                 if st.checkbox("Show Shape"):
@@ -257,17 +324,17 @@ def main():
                     st.write(df.describe())
 
                 if st.checkbox("Show Selected Columns"):
-                    all_columns = show_df.columns.to_list()
+                    all_columns = df.columns.to_list()
                     selected_columns = st.multiselect("Select Columns",all_columns)
                     new_df = df[selected_columns]
                     st.dataframe(new_df)
                     
                 if st.checkbox("Top Rated Mobiles"):
-                    top_rated = show_df.nlargest(10,['Rating'])
+                    top_rated = show_df.nlargest(10,['Total_Ratings'])
                     st.dataframe(top_rated)
                     
                 if st.checkbox("Low Rated Mobiles"):
-                    low_rated = show_df.nsmallest(10,['Rating'],'all')
+                    low_rated = show_df.nsmallest(10,['Total_Ratings'],'all')
                     st.dataframe(low_rated)
                 
                 if st.checkbox('Maximum Rating Count Mobile'):
@@ -294,7 +361,7 @@ def main():
                     min_price = show_df.nsmallest(10, ['Price'], "all")
                     st.dataframe(min_price)
                     
-                if st.checkbox("Drop Selected Columns"):
+                if st.checkbox("Save Selected Columns"):
                     all_columns = df.columns.to_list()
                     selected_columns = st.multiselect("Select Columns to drop",all_columns)
                     clean_data = df.drop(selected_columns, axis = 1)
@@ -302,7 +369,9 @@ def main():
 
 
                     if st.checkbox('Download CSV File'):
-                        filename = st.text_input("Enter File Name: ","")
+                        filename = st.text_input("Enter File Name: ","").strip()
+                        if not filename:
+                            st.warning('Please input a mobile brand name.')
                         if filename !="":
                             filename = filename+'.csv'
                             csv = clean_data.to_csv(index=False)
@@ -314,17 +383,17 @@ def main():
                     
                             
                 if st.checkbox("Show Product Summary"):
-                    st.write('**Length of dataset is **',len(df))
+                    st.write('**Length of dataset is **',len(df['Image_link']))
                     st.write('*Showing First 10 Data...*')
                     try:
                         for i in range(10):
-                            st.image(df['Image Link'][i], width=100, caption=df['Product_Name'][i])
+                            st.image(df['Image_link'][i], width=100, caption=df['Product_Name'][i])
                             st.write('Price Rs. ',df['Price'][i],'Ratings: ',df['Rating'][i])
                             st.write('Total Ratings:',df['Total_Ratings'][i], 'Total Reviews:',df['Total_Reviews'][i])
                             st.markdown("------")
                     except :
                         st.write("**This feature is not available for selected dataset**")
             except KeyError as er:
-                st.write('**Other features are not avalable for uploaded dataset**')
+                st.write('**Some features are not avalable for uploaded dataset**')
 if __name__ == '__main__':
     main()
